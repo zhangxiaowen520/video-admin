2fff95059d61788de11d8ccab66440e945849b5d
ZhangWen <1786261783@qq.com>
2024-12-06 15:46:25 +0800

    chore:更新配置

--- config/config.ts
+++ config/config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   define: {
     API_URL: 'http://www.weiliuyinshi.cn:8080',
   },
+  history: { type: 'hash' },
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
